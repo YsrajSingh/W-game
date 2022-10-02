9dcb4eb0cac38d2122f62ee7bd0d44b67346b8ea
YsrajSingh <ysgahir1925@hotmail.com>
2022-10-02 10:38:19 +0530

    new assignment started of file upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Wgame from './w_game/Wgame';
+import Wgame from './components/w_game/Wgame';
+import Upload from './components/file_upload/Upload';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Wgame/>
+        <Upload/>
       </header>
     </div>
   );
